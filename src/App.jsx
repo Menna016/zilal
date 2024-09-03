e9c4f5cd0173ee63a082f2981bf29653c4f6dfb5
Menna016 <menn90982@gmail.com>
2024-09-03 15:54:44 +0300

    2 commit

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import Home from './Components/Home/Home';
 import About from './Components/About/About';
 import Contact from './Components/Contact/Contact';
 import NotFound from './Components/NotFound/NotFound';
+import Portfolio from './Components/Portfolio/Portfolio';
 
 
 
